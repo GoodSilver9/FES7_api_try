41d200950fcc12cc180b52193207150db216d524
GoodSilver9 <hool784@naver.com>
2023-10-06 13:37:46 +0900

    메인에 로그인페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import LoginPage from "./Components/LoginPage";
+
 function App() {
   // 로그인기능!
-  return <div>hello world</div>;
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
 }
 export default App;
